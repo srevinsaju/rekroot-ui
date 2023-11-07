b60ad1590e035c15d044348dead60dde2a74130b
Srevin Saju <srevinsaju@sugarlabs.org>
2023-11-08 01:56:57 +0300

    feat: add background to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,7 +17,7 @@ export default function AuthenticationPage() {
       <div className="container relative flex min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="absolute inset-0 bg-zinc-900 bg-login-image" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
