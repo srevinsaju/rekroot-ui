00b00b963e3eaf7d22d5f393a96cf1e00f356e10
Srevin Saju <srevinsaju@sugarlabs.org>
2023-11-07 23:46:40 +0300

    fix: add highlight on rekroot title header

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Revolutionize Hiring with <span>rekroot</span>.<br className="hidden sm:inline" />
+          Revolutionize Hiring with <span class="text-primary">rekroot.</span><br className="hidden sm:inline" />{' '}
           Empowering HR Professionals to Streamline Job Postings and Applications
           
         </h1>
